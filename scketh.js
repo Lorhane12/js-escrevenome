4459c6159dcf859057f8bcdf22481df42bbea8a3
Lorhane12 <00001233592063sp@al.educacao.sp.gov.br>
2024-06-11 16:07:14 -0300

    adicionando arquivo js e css

---
+++ scketh.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(800, 800)
+    background("white");
+   }
+   
+   function draw() {
+     
+     stroke("purple")
+     fill("pink")
+     
+    //console.log(mouseIsPressed);
+     
+     if(mouseIsPressed) {       
+      rect(mouseX, mouseY, 20, 35)
+      }
+    }
